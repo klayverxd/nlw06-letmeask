983739969ea5aca06506a43f6fac5c7649f4b7a8
Klayver Ximenes <klayverx@gmail.com>
2021-06-22 18:48:45 -0300

    aula02 - autenticação com firebase

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,24 @@
 import { useHistory } from 'react-router-dom'
 
+import { useAuth } from '../hooks/useAuth'
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 
-import '../styles/auth.scss'
 import { Button } from '../components/Button'
+
+import '../styles/auth.scss'
 
 export function Home() {
 	const history = useHistory()
+	const { user, signInWithGoogle } = useAuth()
 
-	function navigateToNewRoom() {
+	async function handleCreateRoom() {
+		if (!user) {
+			await signInWithGoogle()
+		}
+
 		history.push('rooms/new')
 	}
 
@@ -24,7 +32,7 @@ export function Home() {
 			<main>
 				<div className="main-content">
 					<img src={logoImg} alt="Letmeask" />
-					<button onClick={navigateToNewRoom} className="create-room">
+					<button onClick={handleCreateRoom} className="create-room">
 						<img src={googleIconImg} alt="Logo do Google" />
 						Crie sua sala com o Google
 					</button>
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,12 +1,17 @@
 import { Link } from 'react-router-dom'
 
+// import { useAuth } from '../hooks/useAuth'
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 
-import '../styles/auth.scss'
 import { Button } from '../components/Button'
 
+import '../styles/auth.scss'
+
 export function NewRoom() {
+	// const { user } = useAuth()
+
 	return (
 		<div id='page-auth'>
 			<aside>
